5e40a2489c640fee60ad782c36ab28fe8e4930c8
Lucie KHAMLACH <lkhamlac@e1r12p22.clusters.42paris.fr>
2022-09-20 18:25:32 +0200

    ouioui les angles

--- srcs/angles.c
+++ srcs/angles.c
@@ -29,25 +29,26 @@ void set_angle(t_game *game, int x, int y, char letter)
 // on recupere les angles grace au truc du cercle trigonometrique oe oe oe 
 // https://i2.wp.com/www.methodemaths.fr/cercletrigonometrique.jpg?w=584
 
-void angles_de_ses_morts(t_game game)
+void angles_de_ses_morts(t_game *game)
 {
 	// on init les angles
 	int i;
 	int j;
 
-	while(game->s_map.map[++i])
+	i = 0;
+	while(game->map.map[++i])
 	{
 		j = -1;
-		while(game->s_map.map[++j])
+		while(game->map.map[++j])
 		{
-			if(game->s_map.map[i][j] == 'N')
+			if(game->map.map[i][j] == 'N')
 				set_angle(game, j, i, 'N');
-			else if (game->s_map.map[i][j] == 'S')
-				set_angle(game, j, i, 'S')
-			else if (game->s_map.map[i][j] == 'E')
-				set_angle(game, j, i, 'E')
-			else if (game->s_map.map[i][j] == 'W')
-				set_angle(game, j, i, 'W')
+			else if (game->map.map[i][j] == 'S')
+				set_angle(game, j, i, 'S');
+			else if (game->map.map[i][j] == 'E')
+				set_angle(game, j, i, 'E');
+			else if (game->map.map[i][j] == 'W')
+				set_angle(game, j, i, 'W');
 		}
 	}
 }
